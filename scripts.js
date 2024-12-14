189ee9f1e14938e03a8dacff86c4bbc602e1401d
Jordeval Filho <jordeval.filho@hotmail.com>
2024-12-14 20:11:45 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let imagem = document.querySelector(".imagem-copo")
 let circulo = document.querySelector(".circulo")
+const botao = document.querySelector(".button")
 
 function trocaImagem(endereco){
     imagem.src = endereco
@@ -7,4 +8,8 @@ function trocaImagem(endereco){
 
 function trocaCor(cor){
     circulo.style.background = cor
+}
+
+function redirecionar(pagina) {
+    window.open(pagina, '_blank');
 }
